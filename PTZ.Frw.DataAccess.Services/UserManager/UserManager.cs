09a6c8a56f4abfac8dcf9ed29e5882cf86d637bb
Pedro Torrezão <pedro.torrezao@gmail.com>
2018-02-24 12:11:29 +0000

    Close EntityFramework Dockerized support

--- PTZ.Frw.DataAccess.Services/UserManager/UserManager.cs
+++ PTZ.Frw.DataAccess.Services/UserManager/UserManager.cs
@@ -6,12 +6,16 @@ namespace PTZ.Frw.DataAccess.Services.UserManager
 {
     public class UserManager : IUserManager
     {
+        private readonly PTZFrwContext _context;
+
+        public UserManager(PTZFrwContext ctx)
+        {
+            _context = ctx;
+        }
+
         public User FindByEmail(string userName)
         {
-            using (var context = new PTZFrwContext(new Microsoft.EntityFrameworkCore.DbContextOptions<PTZFrwContext>() { }))
-            {
-                return context.Users.FirstOrDefault(x => x.UserName == userName);
-            }
+            return _context.Users.FirstOrDefault(x => x.UserName == userName);
         }
     }
 }
